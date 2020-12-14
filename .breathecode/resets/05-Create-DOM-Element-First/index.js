30aa66f335009ebbbc5e3957293a4cd83e58ddec
mlanorero <mlanorero@gmail.com>
2020-12-14 22:35:42 +0000

    ejercios Dom

--- .breathecode/resets/05-Create-DOM-Element-First/index.js
+++ .breathecode/resets/05-Create-DOM-Element-First/index.js
@@ -4,7 +4,3 @@ document.querySelector("#myDiv").appendChild(elem);
 
 //Your code here
 // Don'change the code above
-let elemP = document.createElement("P");
-elemP.innerHTML = "Hello World";
-elemP.style.background = "yellow";
-document.body.appendChild(elemP);
--- exercises/06-Create-DOM-Element-Second/index.js
+++ exercises/06-Create-DOM-Element-Second/index.js
@@ -1,3 +1,5 @@
 //you code here
+let elemImage = document.createElement("img");
+elemImage.src = "http://placehold.it/350x150";
 document.body.innerHTML = "<h1>Hello World</h1>";
-
+document.querySelector(".myDiv").appendChild(elemImage);
