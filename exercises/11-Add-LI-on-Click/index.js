80bac5931d9d7c830ebb500e371e3c2a81c68285
mlanorero <mlanorero@gmail.com>
2021-05-15 14:18:49 +0000

    Avanzando

--- exercises/11-Add-LI-on-Click/index.js
+++ exercises/11-Add-LI-on-Click/index.js
@@ -1,6 +1,7 @@
 let button = document.querySelector("#superDuperButton");
 
 button.addEventListener("click", function() {
-	let node = (document.createElement("li").innerHTML = "Forth Element");
+	let node = document.createElement("li");
+	node.innerHTML = "Forth Element";
 	document.querySelector("#myList").appendChild(node); // Append <li> to <ul> with id="myList"
 });
---
+++ exercises/12-Dynamic-HTML-String/index.js
@@ -0,0 +1,3 @@
+let text = document.createElement("p");
+text.innerHTML = ", we are in the year";
+document.querySelector("mystring").appendChild(text);
