aa547573d1fe3b565289a3b47b98e952eb618df7
mlanorero <mlanorero@gmail.com>
2021-05-31 14:39:13 +0000

    faltan 2

--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -3,5 +3,17 @@ window.onload = function() {
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
 	// your code here
-	document.getElementsByTagName("option").innerHTML = countries.length;
+	countries.forEach(myFunction);
+
+	function myFunction(item) {
+		let node = document.createElement("option");
+		document.querySelector("#mySelect").appendChild(node);
+		node.innerHTML = item;
+	}
+
+	document.getElementById("change").addEventListener("onClick", alert);
+
+	function alert() {
+		document.getElementById("change").innerHTML = "hola";
+	}
 };
