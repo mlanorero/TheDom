fafddef588ab88ecb1bdddeab754106a1af0e60d
mlanorero <mlanorero@gmail.com>
2021-06-18 20:54:53 +0000

    to do list

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -6,8 +6,11 @@ input.addEventListener("click", function() {
 	li.innerHTML = "Tarea Agregada";
 	document.querySelector("ul").appendChild(li);
 
-	let n = document.createElement("i class=fa fa-trash");
+	//let i = document.createElement("i");
 	let span = document.createElement("span");
-	span.appendChild(n);
+	var txt = document.createTextNode("\f1f8");
+	span.className = "fa-trash";
+	span.appendChild(txt);
 	li.appendChild(span);
+	//document.querySelector("ul").appendChild(li);
 });
