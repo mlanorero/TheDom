0fda3ba480d5e00375a2b5eb6e7fa2a012d1d18d
mlanorero <mlanorero@gmail.com>
2021-06-19 00:00:37 +0000

    to do list

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,16 +1,26 @@
 // Your code here
 let input = document.querySelector("#addToDo");
+let close = document.getElementsByClassName("fa-trash");
+let i;
 
-input.addEventListener("click", function() {
+function addEventListener() {
 	let li = document.createElement("li");
 	li.innerHTML = "Tarea Agregada";
 	document.querySelector("ul").appendChild(li);
 
-	//let i = document.createElement("i");
+	let icono = document.createElement("i");
 	let span = document.createElement("span");
-	var txt = document.createTextNode("\f1f8");
-	span.className = "fa-trash";
-	span.appendChild(txt);
+	//var txt = document.createTextNode("/f1f8");
+	icono.className = "fa-trash";
+	//i.appendChild(txt);
+	span.appendChild(icono);
 	li.appendChild(span);
-	//document.querySelector("ul").appendChild(li);
-});
+	document.querySelector("ul").appendChild(li);
+
+	for (i = 0; i < close.length; i++) {
+		close[i].onclick = function() {
+			let icono = document.querySelector("i");
+			icono.style.display = "none";
+		};
+	}
+}
